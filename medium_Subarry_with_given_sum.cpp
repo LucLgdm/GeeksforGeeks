1ef8afef19e3dca346eed7cb49572b3ba4b1314c
LucLgdm <149233640+LucLgdm@users.noreply.github.com>
2024-02-08 23:18:10 +0100

    Update medium_Subarry_with_given_sum.cpp

--- medium_Subarry_with_given_sum.cpp
+++ medium_Subarry_with_given_sum.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+/**********************************************************
+    Given an unsorted array A of size N that contains only
+    non negative integers, find a continuous sub-array that
+    adds to a given number S and return the left and right
+    index(1-based indexing) of that subarray.
+
+    In case of multiple subarrays, return the subarray
+    indexes which come first on moving from left to right.
+
+    Note:- You have to return an ArrayList consisting of two
+    elements left and right. In case no such subarray exists
+    return an array consisting of element -1.
+ ************************************************************/
 vector<int> subarraySum(vector<int>arr, int n, long long s)
     {
         int last=0;
