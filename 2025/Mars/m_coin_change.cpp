41ef0a51ee691a0c41b595eb0fe48a54e5852e48
Luc De Mercey <LucLgdm@users.noreply.github.com>
2025-03-14 13:36:54 +0100

    Cleanning

--- 2025/Mars/m_coin_change.cpp
+++ 2025/Mars/m_coin_change.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 using namespace std;
 
+/***************************************************************************************
+ * Given an integer array coins[ ] representing different denominations of currency
+ * and an integer sum, find the number of ways you can make sum by using different
+ * combinations from coins[ ].
+ * Note: Assume that you have an infinite supply of each type of coin.
+ * Therefore, you can use any coin as many times as you want.
+ * Answers are guaranteed to fit into a 32-bit integer. 
+ ***************************************************************************************/
+
 int count(vector<int>& coins, int sum) {
 	// code here.
 	int n = coins.size();
@@ -16,6 +25,7 @@ int count(vector<int>& coins, int sum) {
 }
 
 int count(vector<int>& coins, int sum) {
+	// code here
     int n = coins.size();
     vector<vector<int>> dp(n, vector<int>(sum + 1, 0));
 
