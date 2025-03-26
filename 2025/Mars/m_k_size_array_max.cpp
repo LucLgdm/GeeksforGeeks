e6bdc5b52156f77c8a8750ae61f7dbe3f159ca99
Luc De Mercey <LucLgdm@users.noreply.github.com>
2025-03-26 15:21:09 +0100

    POTD 2 Mars 2025

---
+++ 2025/Mars/m_k_size_array_max.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+	// code here
+	vector<int> result;
+    deque<int> dq; 
+    for (int i = 0; i < arr.size(); ++i) {
+        if (!dq.empty() && dq.front() < i - k + 1) {
+            dq.pop_front();
+        }
+
+        while (!dq.empty() && arr[dq.back()] < arr[i]) {
+            dq.pop_back();
+        }
+
+        dq.push_back(i);
+
+        if (i >= k - 1) {
+            result.push_back(arr[dq.front()]);
+        }
+    }
+
+    return result;
+}
